add363357601628732a12fedda17d502ac87c672
MarcosTort <tort.marcos9@gmail.com>
2023-06-23 00:49:30 -0300

    asd

--- src/colaDePrioridadPersona.cpp
+++ src/colaDePrioridadPersona.cpp
@@ -19,13 +19,6 @@ TFecha obtenerFechaPrioridad(TPersona persona)
 {
   return fechaTEvento(primerEventoDeTPersona(persona));
 }
-nat compararConInversion(TFecha fecha1, TFecha fecha2, bool invertido){
-   if(invertido){
-      return compararTFechas(fecha2, fecha1);
-   }
-   return compararTFechas(fecha1, fecha2);
-}
-
 TColaDePrioridadPersona crearCP(nat N)
 {
   TColaDePrioridadPersona cp = new rep_colaDePrioridadPersona();
@@ -33,7 +26,7 @@ TColaDePrioridadPersona crearCP(nat N)
   cp->tamanio = N;
   cp->cant = 0;
   cp->prioridades = new TFecha[N + 2];
-  cp->array[0].fechaPrioridad = NULL;
+  cp->array[0].fechaPrioridad =NULL;
   cp->array[0].persona = NULL;
   for (nat i = 0; i <= N + 1; i++)
   {
@@ -46,10 +39,15 @@ TColaDePrioridadPersona crearCP(nat N)
   }
   return cp;
 }
-void filtradoAscendente(TColaDePrioridadPersona cp, nat i)
-{
+nat compararConInversion(TFecha fecha1, TFecha fecha2, bool invertido){
+   if(invertido){
+      return compararTFechas(fecha2, fecha1);
+   }
+   return compararTFechas(fecha1, fecha2);
+}
+void filtradoAscendente(TColaDePrioridadPersona cp, nat i){
   
-  while (compararConInversion(cp->array[i / 2].fechaPrioridad, cp->array[i].fechaPrioridad, cp->invertido) && (cp->array[i / 2].persona != NULL))
+  while ((compararTFechas(cp->array[i / 2].fechaPrioridad, cp->array[i].fechaPrioridad) == 1) && (cp->array[i / 2].persona != NULL))
   {
     Elem aux = cp->array[i / 2];
     cp->array[i / 2] = cp->array[i];
@@ -58,15 +56,16 @@ void filtradoAscendente(TColaDePrioridadPersona cp, nat i)
     cp->prioridades[idTPersona(cp->array[i].persona)] = obtenerFechaPrioridad(cp->array[i].persona);
     i = i / 2;
   }
+
 }
 void invertirPrioridad(TColaDePrioridadPersona &cp)
 {
   cp->invertido = !cp->invertido;
   nat tope = cp->tamanio;
-  for (nat i = tope; (nat)tope / 2 >= i; i++)
-  {
+  for(nat i; (nat)tope/2 >= i; i++){
     filtradoAscendente(cp, i);
   }
+ 
 }
 
 void liberarCP(TColaDePrioridadPersona &cp)
@@ -77,10 +76,12 @@ void liberarCP(TColaDePrioridadPersona &cp)
     {
       liberarTPersona(cp->array[i].persona);
     }
+    
   }
   delete[] cp->array;
   delete[] cp->prioridades;
   delete cp;
+  
 }
 
 void insertarEnCP(TPersona persona, TColaDePrioridadPersona &cp)
@@ -91,7 +92,15 @@ void insertarEnCP(TPersona persona, TColaDePrioridadPersona &cp)
   cp->array[cp->cant].fechaPrioridad = obtenerFechaPrioridad(persona);
   cp->prioridades[idTPersona(persona)] = obtenerFechaPrioridad(persona);
   nat i = cp->cant;
-  filtradoAscendente(cp, i);
+  while ((compararTFechas(cp->array[i / 2].fechaPrioridad, cp->array[i].fechaPrioridad) == 1) && (cp->array[i / 2].persona != NULL))
+  {
+    Elem aux = cp->array[i / 2];
+    cp->array[i / 2] = cp->array[i];
+    cp->prioridades[idTPersona(cp->array[i / 2].persona)] = obtenerFechaPrioridad(cp->array[i / 2].persona);
+    cp->array[i] = aux;
+    cp->prioridades[idTPersona(cp->array[i].persona)] = obtenerFechaPrioridad(cp->array[i].persona);
+    i = i / 2;
+  }
 }
 
 bool estaVaciaCP(TColaDePrioridadPersona cp)
@@ -101,28 +110,25 @@ bool estaVaciaCP(TColaDePrioridadPersona cp)
 
 TPersona prioritaria(TColaDePrioridadPersona cp)
 {
-
+  
   return cp->array[1].persona;
 }
+
 void eliminarPrioritaria(TColaDePrioridadPersona &cp)
 {
-  if (estaVaciaCP(cp))
-  {
+  if(estaVaciaCP(cp)){
     return;
   }
   Elem min = cp->array[1];
   Elem ult = cp->array[cp->cant];
   nat iter = 1;
   nat hijo = iter * 2;
-  for (iter = 1; iter * 2 <= cp->cant; iter = hijo)
-  {
+  for(iter = 1; iter*2 <= cp->cant; iter = hijo){
     hijo = iter * 2;
-    if (hijo != cp->cant && compararConInversion(cp->array[hijo].fechaPrioridad, cp->array[hijo + 1].fechaPrioridad, cp->invertido))
-    {
+    if(hijo != cp->cant && compararTFechas(cp->array[hijo].fechaPrioridad, cp->array[hijo + 1].fechaPrioridad) == 1){
       hijo++;
     }
-    if (compararTFechas(cp->array[hijo].fechaPrioridad, ult.fechaPrioridad) == 1)
-    {
+    if(compararTFechas(cp->array[hijo].fechaPrioridad, ult.fechaPrioridad) == 1){
       break;
     }
     cp->array[iter] = cp->array[hijo];
@@ -136,7 +142,7 @@ void eliminarPrioritaria(TColaDePrioridadPersona &cp)
 
 bool estaEnCP(nat id, TColaDePrioridadPersona cp)
 {
-
+  
   return cp->prioridades[id] != NULL;
 }
 
