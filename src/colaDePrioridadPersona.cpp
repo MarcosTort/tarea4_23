f3f4c0210f852b1729f16143f2dca9a774cf5b18
MarcosTort <tort.marcos9@gmail.com>
2023-06-23 00:47:08 -0300

    asd

--- src/colaDePrioridadPersona.cpp
+++ src/colaDePrioridadPersona.cpp
@@ -63,7 +63,7 @@ void invertirPrioridad(TColaDePrioridadPersona &cp)
 {
   cp->invertido = !cp->invertido;
   nat tope = cp->tamanio;
-  for (nat i; (nat)tope / 2 >= i; i++)
+  for (nat i = tope; (nat)tope / 2 >= i; i++)
   {
     filtradoAscendente(cp, i);
   }
